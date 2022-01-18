ea2e5a9c86135ee26c9fe356e3fc26345ce6a4ea
ufuckee <vip@pornhub.cpm>
2022-01-18 12:07:57 +0800

    update

--- rongqi.go
+++ rongqi.go
@@ -14,43 +14,93 @@ func initRongQi() {
 			Rules: []string{"迁移"},
 			Admin: true,
 			Handle: func(s core.Sender) interface{} {
-				//去重
-				// var mc = map[string]string{}                       //记录ck对应的clientId
-				var mcks = map[*qinglong.QingLong][]qinglong.Env{} //分组记录ck
+				if it := s.GetImType(); it != "terminal" && it != "tg" {
+					return "可能会产生大量消息，请在终端或tg进行操作。"
+				}
+				//容器内去重
+				var memvs = map[*qinglong.QingLong][]qinglong.Env{} //分组记录ck
+				var aggregated = []*qinglong.QingLong{}
 				for _, ql := range qinglong.GetQLS() {
-					tail := fmt.Sprintf("	——来自%s", ql.Name)
+					if ql.AggregatedMode {
+						aggregated = append(aggregated, ql)
+					}
 					envs, err := qinglong.GetEnvs(ql, "JD_COOKIE")
 					if err == nil {
-						// if !ql.AggregatedMode {
 						var mc = map[string]bool{}
 						nn := []qinglong.Env{}
 						for _, env := range envs {
 							if env.Status == 0 {
-								pt_pin := core.FetchCookieValue(env.Value, "pt_pin")
-								name, _ = url.QueryUnescape(pt_pin)
-								if _, ok := mc[pt_pin]; ok {
+								env.PtPin = core.FetchCookieValue(env.Value, "pt_pin")
+								if env.PtPin == "" {
+									continue
+								}
+								name, _ = url.QueryUnescape(env.PtPin)
+								if _, ok := mc[env.PtPin]; ok {
 									if _, err := qinglong.Req(ql, qinglong.PUT, qinglong.ENVS, "/disable", []byte(`["`+env.ID+`"]`)); err == nil {
-										s.Reply(fmt.Sprintf("发现到重复账号，已隐藏(%s)%s。", name, tail))
+										s.Reply(fmt.Sprintf("在同一容器发现到重复账号，已隐藏(%s)%s。", name, ql.GetTail()))
 									}
 									env.Remarks = "重复账号。"
 									qinglong.UdpEnv(ql, env)
 								} else {
-									mc[pt_pin] = true
+									mc[env.PtPin] = true
 									nn = append(nn, env)
 								}
 							}
 						}
-						mcks[ql] = nn
-						// } else {
-
-						// }
-
+						memvs[ql] = nn
 					}
 				}
+				//容器间去重
+				var eql = map[string]*qinglong.QingLong{}
+				for ql, envs := range memvs {
+					if ql.AggregatedMode {
+						continue
+					}
+					nn := []qinglong.Env{}
+					for _, env := range envs {
+						name, _ = url.QueryUnescape(env.PtPin)
+						if _, ok := eql[env.PtPin]; ok {
+							if ql_, err := qinglong.Req(ql, qinglong.PUT, qinglong.ENVS, "/disable", []byte(`["`+env.ID+`"]`)); err == nil {
+								s.Reply(fmt.Sprintf("在%s发现重复账号，已隐藏(%s)%s。", ql.GetName(), name, ql_.GetTail()))
+							}
+							env.Remarks = "重复账号。"
+							qinglong.UdpEnv(ql, env)
+						} else {
+							eql[env.PtPin] = ql
+							nn = append(nn, env)
+						}
+					}
+					memvs[ql] = nn
+				}
 				//聚合
-				//均匀
+				if len(aggregated) > 0 {
+					for _, aql := range aggregated {
+						toapp := []qinglong.Env{}
+						for ql, envs := range memvs {
+							if ql == aql {
+								continue
+							}
+							for _, env := range envs {
+								if !envContain(append(memvs[aql], toapp...), env) {
+									toapp = append(toapp, env)
+								}
+							}
+						}
+						memvs[aql] = append(memvs[aql], toapp...)
+						qinglong.AddEnv(aql, toapp...)
+					}
+				}
 				return "迁移完成。"
 			},
 		},
 	})
+}
+
+func envContain(ay []qinglong.Env, e qinglong.Env) bool {
+	for _, v := range ay {
+		if v.PtPin == e.PtPin {
+			return true
+		}
+	}
+	return false
 }
